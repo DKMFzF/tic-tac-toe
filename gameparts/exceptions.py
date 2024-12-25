330fc059d57bcbe9e61b3e302058cb300d53c863
DKMFzF <dorohev.kir@gmail.com>
2024-12-25 16:34:43 +0700

    feat(relis)

---
+++ gameparts/exceptions.py
@@ -0,0 +1,11 @@
+class FieldIndexError(IndexError):
+    """ Выбрасывается, если выбрано значение вне поля. """
+
+    def __init__(self, messege='Введено значение за границами игрового поля!'):
+        super().__init__(messege)
+
+class CellOccupiedError(Exception):
+    """ Выбрасывается, если выбрано значение уже занято. """
+
+    def __init__(self, message='Попытка изменить занятую ячейку'):
+        super().__init__(message)
