632eb5a7ca4786ef8b26c659007524d212bf97c2
DKMFzF <dorohev.kir@gmail.com>
2024-12-25 17:26:36 +0700

    feat(game.py): formatin code

--- game.py
+++ game.py
@@ -3,8 +3,7 @@ from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 
 def save_result(result):
-    # Если нужно явно указать кодировку, добавьте параметр encoding='utf-8'.
-    with open('results.txt', 'a') as f:
+    with open('results.txt', 'a', encoding='utf-8') as f:
         f.write(result + '\n')
 
 
@@ -21,13 +20,13 @@ def main():
         while True:
             try:
                 row = int(input('Введите номер строки: '))
-                if row < 0 or row >= game.field_size:
-                    raise FieldIndexError
+                if row < 0 or row >= game.field_size: raise FieldIndexError
+                
                 column = int(input('Введите номер столбца: '))
-                if column < 0 or column >= game.field_size:
-                    raise FieldIndexError
-                if game.board[row][column] != ' ':
-                    raise CellOccupiedError
+                if column < 0 or column >= game.field_size: raise FieldIndexError
+                
+                if game.board[row][column] != ' ': raise CellOccupiedError
+            
             except FieldIndexError:
                 print(
                     'Значение должно быть неотрицательным и меньше '
@@ -35,16 +34,20 @@ def main():
                 )
                 print('Введите значения для строки и столбца заново.')
                 continue
+            
             except CellOccupiedError:
                 print('Ячейка занята.')
                 print('Введите другие координаты.')
                 continue
+            
             except ValueError:
                 print('Буквы вводить нельзя. Только числа.')
                 print('Введите значения для строки и столбца заново.')
                 continue
+            
             except Exception as e:
                 print(f'Возникла ошибка: {e}')
+            
             else:
                 break
 
